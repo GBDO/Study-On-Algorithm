cfb6c4fc4eb3c735efe1bcd63077d0488ff1f56a
GBDO <dogibum6@gmail.com>
2022-05-14 21:38:12 +0900

    Update test_baekjoon_10825.py

--- test_baekjoon_10825.py
+++ test_baekjoon_10825.py
@@ -9,11 +9,14 @@ Original file is located at
 
 def scoresort(num, scores):
     ls = []
+    result = []
     for i in range(num):
         [name, kor, eng, math]= map(str, scores[i].split())
         ls.append([name, int(kor), int(eng), int(math)])
     ls.sort(key = lambda x: (-x[1],x[2],-x[3],x[0]))
     for i in range(num):
-        return (ls[i][0])
+        result.append(ls[i][0])
+    return result
+
 def test_scoresort():
-   assert scoresort(12, ['Junkyu 50 60 100','Sangkeun 80 60 50','Sunyoung 80 70 100','Soong 50 60 90','Haebin 50 60 100','Kangsoo 60 80 100','Donghyuk 80 60 100','Sei 70 70 70','Wonseob 70 70 90','Sanghyun 70 70 80','nsj 80 80 80','Taewhan 50 60 90']) == 'Donghyuk','Sangkeun','Sunyoung','nsj','Wonseob','Sanghyun','Sei','Kangsoo','Haebin','Junkyu','Soong','Taewhan'
+   assert scoresort(12, ['Junkyu 50 60 100','Sangkeun 80 60 50','Sunyoung 80 70 100','Soong 50 60 90','Haebin 50 60 100','Kangsoo 60 80 100','Donghyuk 80 60 100','Sei 70 70 70','Wonseob 70 70 90','Sanghyun 70 70 80','nsj 80 80 80','Taewhan 50 60 90']) == ['Donghyuk','Sangkeun','Sunyoung','nsj','Wonseob','Sanghyun','Sei','Kangsoo','Haebin','Junkyu','Soong','Taewhan']
