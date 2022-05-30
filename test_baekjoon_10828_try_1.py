a6f7e6de7eeeecdfff00c7cf04d9dfdc2fbe101c
GBDO <dogibum6@gmail.com>
2022-05-30 23:08:36 +0900

    Update test_baekjoon_10828_try_1.py

--- test_baekjoon_10828_try_1.py
+++ test_baekjoon_10828_try_1.py
@@ -35,12 +35,12 @@ def stackf(stack,input):
         else:
             return -1
 
-def test_stack():
+def test_stackf():
     stack = []
     result = []
     ls = ['push 1','push 2','top','size','empty','pop','pop','pop','size','empty','pop','push 3','empty','top']
     for k in ls:
-        x = stack(stack,k)
+        x = stackf(stack,k)
         if len(ls.split()) > 1:
             result.append(x)
     assert result == [2,2,0,2,1,-1,0,1,-1,0,3]
