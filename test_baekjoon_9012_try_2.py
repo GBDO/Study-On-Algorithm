7657273f3cca35f70e02a5c65e5cdb444a2e3266
GBDO <dogibum6@gmail.com>
2022-06-01 21:52:35 +0900

    Update test_baekjoon_9012_try_2.py

--- test_baekjoon_9012_try_2.py
+++ test_baekjoon_9012_try_2.py
@@ -24,6 +24,7 @@ def VPS(tarls):
         if len(temp) == 0:
             result += "YES "
         else: result += "NO "
+        return result
 
 tarls = ['(())())','(((()())()','(()())((()))','((()()(()))(((())))()','()()()()(()()())()','(()((())()(']
 
