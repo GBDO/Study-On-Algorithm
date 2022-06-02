5af1addff157b9551c3d875cb1f1afdd244264c0
GBDO <dogibum6@gmail.com>
2022-06-02 22:42:51 +0900

    Update test_baekjoon_4949.py

--- test_baekjoon_4949.py
+++ test_baekjoon_4949.py
@@ -7,10 +7,10 @@ Original file is located at
     https://colab.research.google.com/drive/1ze1qed9OKzbvNSd_TKD5k2TAFZkCpVX3
 """
 
-def balance(ch):
+def balance(inp):
     stack = [] 
     flag = 0
-    for c in ch:
+    for c in inp:
         if c == '(':
             stack.append(c)
         elif c == '[':
@@ -29,13 +29,12 @@ def balance(ch):
 
 def test_balance():
     ch = [
-        'So when I die (the [first] I will see in (heaven) is a score list).',
+    'So when I die (the [first] I will see in (heaven) is a score list).',
     '[ first in ] ( first out ).',
     'Half Moon tonight (At least it is better than no Moon at all].',
     'A rope may form )( a trail in a maze.',
     'Help( I[m being held prisoner in a fortune cookie factory)].',
-    '([ (([( [ ] ) ( ) (( ))] )) ]).',
-    ' .'
+    '([ (([( [ ] ) ( ) (( ))] )) ]).'
     ]
     result = ''
     for inp in ch:
